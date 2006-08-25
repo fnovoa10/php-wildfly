5464fd808a5f93f314b4c18eb06841e6d474e1c9
jfrederic.clere@jboss.com <jfrederic.clere@jboss.com@0a86c6aa-4018-0410-a694-dd168e64b0d7>
2006-08-25 15:18:07 +0000

    Upgrade php to 5.1.5 Arrange location of demo. Add back the servlet definitions under different names. (demo...)

--- trunk/php/buildphp.sh
+++ trunk/php/buildphp.sh
@@ -9,7 +9,7 @@
 # There are windoze binaries
 #
 
-PHPVER=5.1.4
+PHPVER=5.1.5
 PHPURL=http://de.php.net/distributions/php-${PHPVER}.tar.gz
 
 XML2VER=2.6.24
@@ -828,14 +828,16 @@ fi
 #
 # Build the war file with the demo
 ant build-demo
-cp -p php-examples.war $TOOLS
+mkdir -p $TOOLS/server/default/deploy/jbossweb.sar
+cp -p php-examples.war $TOOLS/server/default/deploy/jbossweb.sar
 
 #
 # Build a tarball with the all stuff
 (cd  $TOOLS/
  rm -f php5servlet.tar.gz
  rm -f php5servlet.tar
- tar cvf php5servlet.tar PHP/lib webapps
+ tar cvf php5servlet.tar PHP/lib server/default/deploy/jbossweb.sar/php-examples.war
  gzip -9 php5servlet.tar
 )
 
+echo "Done: $TOOLS/php5servlet.tar.gz"
