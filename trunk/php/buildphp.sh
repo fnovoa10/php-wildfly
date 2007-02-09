2f5ba468975d845639ab5fde1030773fcfb5caf0
jfrederic.clere@jboss.com <jfrederic.clere@jboss.com@0a86c6aa-4018-0410-a694-dd168e64b0d7>
2007-02-09 17:28:13 +0000

    Upgrade php and postgres.

--- trunk/php/buildphp.sh
+++ trunk/php/buildphp.sh
@@ -9,13 +9,13 @@
 # There are windoze binaries
 #
 
-PHPVER=5.1.5
+PHPVER=5.2.1
 PHPURL=http://de.php.net/distributions/php-${PHPVER}.tar.gz
 
 XML2VER=2.6.24
 XML2URL=ftp://xmlsoft.org/libxml2/libxml2-${XML2VER}.tar.gz
 
-PSQLVER=8.1.3
+PSQLVER=8.1.6
 PSQLURL=http://wwwmaster.postgresql.org/redir?ftp://ftp2.ch.postgresql.org/pub/postgresql/source/v${PSQLVER}/postgresql-${PSQLVER}.tar.gz
 
 OSSLVER="0.9.8b"
